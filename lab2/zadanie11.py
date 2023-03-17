6b3ca468c79a4b87f76b77cd4c869c2fc75925ed
Pawel Szum <pwlgsz26@gmail.com>
2023-03-17 12:13:31 +0100

    Laboratorium 2

--- lab2/zadanie11.py
+++ lab2/zadanie11.py
@@ -7,17 +7,21 @@ if __name__ == '__main__':
     client_socket.settimeout(5)
 
     try:
-        client_socket.connect(('212.182.24.27', 2900))
+        client_socket.connect(('212.182.24.27', 2908))
         print('Connected')
     except socket.error as e:
         print('socket.error exception: %s' % e)
         sys.exit()
 
     message = input('> ')
+    while len(message) < 20:
+        message += ' '
+    if len(message) > 20:
+        message = message[:20]
 
     client_socket.send(message.encode())
 
-    data = client_socket.recv(4096).decode()
+    data = client_socket.recv(20).decode()
     print(data)
 
     client_socket.close()
