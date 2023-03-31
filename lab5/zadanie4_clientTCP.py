5484d7bac0404c8d8c128e49170e3d5db5ff3796
Pawel Szum <pwlgsz26@gmail.com>
2023-03-31 11:59:27 +0200

    Laboratorium 5

---
+++ lab5/zadanie4_clientTCP.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import socket
+import sys
+
+if __name__ == '__main__':
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.settimeout(5)
+
+    try:
+        client_socket.connect(('127.0.0.1', 6012))
+        print('Connected')
+    except socket.error as e:
+        print('socket.error exception: %s' % e)
+        sys.exit()
+
+    message = 'Hello world!'
+    client_socket.send(message.encode())
+
+    data = client_socket.recv(4096).decode()
+    print(data)
+
+    client_socket.close()
